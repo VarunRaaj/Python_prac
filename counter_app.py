2c5d61c5b152c4c5e11d347a272090df04f2eb8e
Varun Raaj <vraj.d.7899@gmail.com>
2024-04-27 04:28:45 -0700

    demo

---
+++ counter_app.py
@@ -0,0 +1,19 @@
+"""
+#My Attempt
+print('Welcome to the letter counter App')
+Username=input("Please Enter your name :")
+print("hello " +Username +" !")
+print('I will count the number of occurences , that specific letter you give occurs in a Stentence')
+Sentence = input('Please enter the message ') 
+Letter= input('Enter the letter you want to find the occurance in the sentence')
+count=0
+for i in Sentence:
+    if i==Letter:
+        count=count+1
+        
+print("Count of the letter" +Letter+ " in the sentence is ")   
+print(count) 
+"""
+#Actual solution
+
+     
